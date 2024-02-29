ca0bde2280f805aff649623e3a9972165b264529
ansorokey <ansorokey@gmail.com>
2024-02-29 13:03:10 -0500

    connected convex to clerk jwt

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://decent-wren-67.clerk.accounts.dev", // from Clerk JWT page
+        applicationID: "convex", // must match AUD from clerk JWT
+      },
+    ]
+  };
