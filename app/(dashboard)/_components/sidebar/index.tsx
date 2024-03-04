e992c0cfb97d35295ff06c7f0d39c0686ed8093d
ansorokey <ansorokey@gmail.com>
2024-03-04 15:08:03 -0500

    added basic styling to sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,8 @@
 export const Sidebar = () => {
     return (
-        <div>
+        <aside className="fixed z-[1] left-0 bg-blue-950
+        h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
             sidebar
-        </div>
+        </aside>
     );
 }
