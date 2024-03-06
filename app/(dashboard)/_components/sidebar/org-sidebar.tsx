ed66bef62f7cd79dabbe9bc7bbd2fc90de7da0d7
ansorokey <ansorokey@gmail.com>
2024-03-06 14:36:02 -0500

    organization bar added, shows on switch

--- app/(dashboard)/_components/sidebar/org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/org-sidebar.tsx
@@ -1,11 +1,40 @@
 "use client";
 // a client component allows us to use hooks, useEffects, onCL=licks, other interactive elements
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+});
+
 export const OrgSidebar = () => {
     return (
         // hidden on small screen
         <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
-            Org Sidebar
+            <Link href="">
+                <div className="flex items-center gap-x-2">
+                    <Image
+                        src="/logo.svg"
+                        alt="Logo"
+                        height={60}
+                        width={60}
+                    />
+                    <span className={cn(
+                        "font-semibold text-2xl",
+                        font.className
+                    )}>
+                        Board
+                    </span>
+                </div>
+            </Link>
+            <OrganizationSwitcher
+
+            />
         </div>
     );
 }
