f66f27336680b9d880fa58535abfe3b5ab3afc81
ansorokey <ansorokey@gmail.com>
2024-03-06 14:43:45 -0500

    added and styled team board sbutton

--- app/(dashboard)/_components/sidebar/org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/org-sidebar.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard } from "lucide-react";
 
 const font = Poppins({
     subsets: ["latin"],
@@ -33,8 +35,38 @@ export const OrgSidebar = () => {
                 </div>
             </Link>
             <OrganizationSwitcher
-
+                hidePersonal
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%"
+                        },
+                        organizationSwitcherTrigger: {
+                            padding: "6px",
+                            width: "100%",
+                            borderRadius: "8px",
+                            border: "1px solid #E5E7EB",
+                            justifyContent: "space-between",
+                            backgroundColor: "white"
+                        }
+                    }
+                }}
             />
+            <div className="space-y-1 w-full">
+                <Button
+                    asChild
+                    size="lg"
+                    className="font-normal justify-start px-2 w-full"
+                >
+                    <Link href="/">
+                        <LayoutDashboard className="h-4 w-4 mr-2" />
+                        Team Boards
+                    </Link>
+                </Button>
+            </div>
         </div>
     );
 }
