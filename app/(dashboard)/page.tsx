2212a65b7e3186203b2d0f1e12bb39b6a5461d88
ansorokey <ansorokey@gmail.com>
2024-03-10 06:35:03 -0400

    cleaning up styling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+    searchParams: {
+        search?: string;
+        favorites?: string;
+    }
+}
+
+const DashboardPage = ({
+    searchParams
+}: DashboardPageProps) => {
+    const { organization } = useOrganization();
+
     return (
         <div className="flex-1 h-[calc(100%-80px)] p-6">
-            <EmptyOrg />
+            {JSON.stringify(searchParams)}
+            {!organization ? (
+                <EmptyOrg />
+            ): (
+                <BoardList />
+            )}
         </div>
     );
 }
