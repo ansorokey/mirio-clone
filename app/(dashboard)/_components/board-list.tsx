0ffdeba6205124a1cb6ccbd74d7a62c7a477932a
ansorokey <ansorokey@gmail.com>
2024-03-13 13:00:27 -0400

    baord-list complete

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -24,12 +24,13 @@ export const BoardList = ({
 
     // If the data doesnt exist, convex would return null specifically
     // Undefined means data is loading
-    if(true) {
+    if(data === undefined) {
         return (
             <div>
                 <h2 className="text-3xl">
                     {query.favorites ? "Favorite boards" : "Team Boards"}
                 </h2>
+                {/* empty skeleton templates, add any arbitrary amount */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
                     <NewBoardButton orgId={orgId} disabled />
                     <BoardCard.Skeleton />
