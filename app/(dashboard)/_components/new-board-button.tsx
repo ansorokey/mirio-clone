3dc8a67640e4d22e8a05329f4845043268b53f7f
ansorokey <ansorokey@gmail.com>
2024-03-13 11:07:41 -0400

    board creation toast

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface NewBoardButtonProps {
     orgId: string;
@@ -20,6 +21,13 @@ export const NewBoardButton = ({
             orgId,
             title: "Untitled"
         })
+            .then((id) =>{
+                toast.success("Board created")
+                // TODO: redirect to board id
+            })
+            .catch(() => {
+                toast.error("Failed to create board")
+            })
     }
     return (
         <button
