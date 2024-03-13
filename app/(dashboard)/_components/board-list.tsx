5cdf9a9a2ed5dfadb926a97344e882c5bef0033f
ansorokey <ansorokey@gmail.com>
2024-03-13 12:57:20 -0400

    board card loading skeleton

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -24,10 +24,19 @@ export const BoardList = ({
 
     // If the data doesnt exist, convex would return null specifically
     // Undefined means data is loading
-    if(data === undefined) {
+    if(true) {
         return (
             <div>
-                Loading...
+                <h2 className="text-3xl">
+                    {query.favorites ? "Favorite boards" : "Team Boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                    <NewBoardButton orgId={orgId} disabled />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                </div>
             </div>
         )
     }
