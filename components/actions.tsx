c92073023ce2fc83c3e00e9bb95b7c5fa9dfb9cb
ansorokey <ansorokey@gmail.com>
2024-03-14 10:19:24 -0400

    link copy bitton and toast

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -8,11 +8,13 @@ import {
     DropdownMenuSeparator,
     DropdownMenuItem
 } from "@/components/ui/dropdown-menu";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionsProps {
     children: React.ReactNode;
-    side?: DropdownMenuProps["side"];
-    sideOffset?: DropdownMenuProps["sideOffset"];
+    side?: DropdownMenuContentProps["side"];
+    sideOffset?: DropdownMenuContentProps["sideOffset"];
     id: string;
     title: string;
 }
@@ -24,11 +26,36 @@ export const Actions = ({
     id,
     title
 }: ActionsProps) => {
+    const onCopyLink = () => {
+        navigator.clipboard.writeText(
+            `${window.location.origin}/board/${id}`
+        )
+            .then(() => toast.success("Link copied"))
+            .catch(() => toast.error("Failed to copy link"))
+    }
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 {children}
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+                // prevents the link beneath the options from redirecting
+                onClick={(e) => e.stopPropagation()}
+                side={side}
+                sideOffset={sideOffset}
+                className="w-60"
+            >
+                <DropdownMenuItem
+                    className="p-3 cursor-pointer"
+                    onClick={onCopyLink}
+                >
+                    <Link2
+                        className="h-4 w-4 mr-2"
+                    />
+                    Copy board link
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 }
