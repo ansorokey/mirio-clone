40565a93a807fcfd281741d07eb24a1a71dff383
ansorokey <ansorokey@gmail.com>
2024-03-19 14:58:29 -0400

    can favorite boards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,6 +9,9 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface BoardCardProps {
     id: string;
@@ -32,6 +35,27 @@ const BoardCard = ({
     isFavorite
 }: BoardCardProps) => {
     const { userId } = useAuth();
+
+    // alias these destructured functions/variables for namespace
+    const {
+        mutate: onFavorite,
+        pending: pendingFavorite
+    } = useApiMutation(api.board.favorite);
+    const {
+        mutate: onUnfavorite,
+        pending: pendingUnfavorite
+    } = useApiMutation(api.board.unfavorite);
+
+    const toggleFavorite = () => {
+        if(isFavorite) {
+            onUnfavorite({ id })
+                .catch(() => toast.error("Failed to unfavorite"));
+        } else {
+            onFavorite({ id, orgId })
+                .catch(() => toast.error("Failed to favorite"));
+        }
+
+    }
 
     const authorLabel = userId == authorId ? "You" : authorName;
     const createdAtLabel = formatDistanceToNow(createdAt, {
@@ -58,7 +82,8 @@ const BoardCard = ({
                             className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
                         >
                             <MoreHorizontal
-                                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                                className="text-white opacity-75 hover:opacity-100
+                                transition-opacity"
                             />
                         </button>
                     </Actions>
@@ -68,8 +93,8 @@ const BoardCard = ({
                     title={title}
                     authorLabel={authorLabel}
                     createdAtLabel={createdAtLabel}
-                    onClick={() => {}}
-                    disabled={false}
+                    onClick={toggleFavorite}
+                    disabled={pendingFavorite || pendingUnfavorite}
                 />
             </div>
         </Link>
