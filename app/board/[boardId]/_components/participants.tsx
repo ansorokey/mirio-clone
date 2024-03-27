d90f09b831edbc0fe3cc65ed757cd8dd506d1eba
ansorokey <ansorokey@gmail.com>
2024-03-27 13:06:51 -0400

    participant slight styling

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,7 @@
 export const Participants = () => {
     return (
-        <div>
-            Particiipants
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+            List of users
         </div>
     );
 };
