0696454c46ce90cefc9b5d37070c74b828b152d4
ansorokey <ansorokey@gmail.com>
2024-04-01 15:16:15 -0400

    removed skeleton imports

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,5 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export const Participants = () => {
     return (
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
@@ -12,8 +10,6 @@ export const Participants = () => {
 Participants.Skeleton = function ParticipantsSkeleton() {
     return (
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md
-        p-3 flex items-center shadow-md w-[100px]">
-            <Skeleton className="h-full w-full bg-muted-400" />
-        </div>
+        p-3 flex items-center shadow-md w-[100px]" />
     );
 }
