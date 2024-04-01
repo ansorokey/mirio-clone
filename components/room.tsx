8216648fc0685d917ab554821b821afa1590ce38
ansorokey <ansorokey@gmail.com>
2024-04-01 14:55:34 -0400

    basic live room component

---
+++ components/room.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import { RoomProvider } from "@/liveblocks.config";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+interface RoomProps {
+    children: ReactNode;
+    roomId: string;
+}
+
+
+export const Room = ({
+    children,
+    roomId
+}: RoomProps) => {
+    return (
+        <RoomProvider id={roomId} initialPresence={{}}>
+            <ClientSideSuspense fallback={<div>Loading...</div>}>
+                {() => children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    );
+}
