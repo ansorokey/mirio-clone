6176b5611dfd6fee23116bedd8436eff5bb1c0f8
ansorokey <ansorokey@gmail.com>
2024-04-01 15:04:12 -0400

    reusable canvas loading component

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,5 +1,6 @@
 import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
+import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
     params: {
@@ -13,8 +14,8 @@ const BoardIdPage = ({
 }: BoardIdPageProps) => {
     return (
         <div className="h-full">
-            {/* OPening a board triggers a new room on liveblocks */}
-            <Room roomId={params.boardId} fallback={<div>Loading...</div>}>
+            {/* Opening a board triggers a new room on liveblocks */}
+            <Room roomId={params.boardId} fallback={<Loading />}>
                 <Canvas boardId={params.boardId} />
             </Room>
         </div>
