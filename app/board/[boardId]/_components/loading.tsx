a7f3e6e8cb2390f509c54084ae42ae840e8a1bb0
ansorokey <ansorokey@gmail.com>
2024-04-01 15:14:10 -0400

    toolbar skeleton on canvas loading page

--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -2,6 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Loader } from "lucide-react";
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
 
 export const Loading = () => {
     return (
@@ -9,6 +10,7 @@ export const Loading = () => {
             <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
             <Info.Skeleton />
             <Participants.Skeleton />
+            <Toolbar.Skeleton />
         </main>
     );
 }
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Toolbar = () => {
     return (
         <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -12,6 +14,14 @@ export const Toolbar = () => {
                 <div>Undo</div>
                 <div>Redo</div>
             </div>
+        </div>
+    );
+}
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+    return (
+        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md">
+            <Skeleton className="h-full w-full bg-muted-400" />
         </div>
     );
 }
