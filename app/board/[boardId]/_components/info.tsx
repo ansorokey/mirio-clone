6af58500580fa58ed07082786ed0d60d8b5d29dd
ansorokey <ansorokey@gmail.com>
2024-04-02 18:50:49 -0400

    logo on board page returns to dashboard

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 interface InfoProps {
@@ -31,14 +32,23 @@ export const Info = ({
 
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-           <Button className="px-2">
-             <Image
-                src="/logo.svg"
-                alt="Site Logo"
-                height={40}
-                width={40}
-             />
-           </Button>
+            <Button asChild className="px-2" variant="board">
+                <Link href="/">
+                    <Image
+                        src="/logo.svg"
+                        alt="Site Logo"
+                        height={40}
+                        width={40}
+                    />
+
+                    <span className={cn(
+                        "font-semibold text-xl ml-2 text-black",
+                        font.className
+                    )}>
+                        Board
+                    </span>
+                </Link>
+            </Button>
         </div>
     );
 };
