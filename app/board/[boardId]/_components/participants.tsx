594e0f3dbe4a91fe07b3a16fdcb302f6a62eeb82
ansorokey <ansorokey@gmail.com>
2024-04-02 19:28:31 -0400

    users avatars and names listed on board

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { currentUser } from "@clerk/nextjs";
 import { UserAvatar } from "./user-avatar";
 import { useOthers, useSelf } from "@/liveblocks.config";
 
@@ -7,12 +8,13 @@ const MAX_SHOWN_USERS = 2;
 
 export const Participants = () => {
     const users = useOthers();
-    const currrentUsers = useSelf();
+    const currentUser = useSelf();
     const hasMoreUsers = users.length > MAX_SHOWN_USERS;
 
     return (
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
             <div className="flex gap-x-2">
+                {/* Users other than current user */}
                 {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info}) => {
                     return (
                         <UserAvatar
@@ -24,6 +26,23 @@ export const Participants = () => {
                     );
                 })}
 
+                {/* This user's name and avatar */}
+                {currentUser && (
+                    <UserAvatar
+                        src={currentUser.info?.picture}
+                        name={`${currentUser.info?.name} (You)`}
+                        fallback={currentUser.info?.name?.[0]}
+                    />
+                )}
+
+                {/* If there are more other users than given max,
+                show avatar with number as that says 'X more' */}
+                {hasMoreUsers && (
+                    <UserAvatar
+                        name={`${users.length - MAX_SHOWN_USERS} more`}
+                        fallback={`${+users.length + MAX_SHOWN_USERS}`}
+                    />
+                )}
             </div>
         </div>
     );
