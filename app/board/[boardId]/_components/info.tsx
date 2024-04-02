f55d9822e23367244aa551067a95c8e8cdbe72cf
ansorokey <ansorokey@gmail.com>
2024-04-02 18:45:13 -0400

    added logo to info on board page

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,18 +1,44 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+
 interface InfoProps {
     boardId: string;
-
 }
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+});
 
 export const Info = ({
     boardId
 }: InfoProps) => {
-    const oard =
+    const data = useQuery(api.board.get, {
+        id: boardId as Id<"boards">
+    });
+
+    if(!data) {
+        return <InfoSkeleton />
+    }
 
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-           TODO: Information about the board
+           <Button>
+             <Image
+                src="/logo.svg"
+                alt="Site Logo"
+                height={40}
+                width={40}
+             />
+           </Button>
         </div>
     );
 };
