acfb2e9ec70248e709dff503da79180eeb605eb8
ansorokey <ansorokey@gmail.com>
2024-04-02 18:39:00 -0400

    info interface and props

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -13,11 +13,12 @@ interface CanvasProps {
 export const Canvas = ({
     boardId
 }: CanvasProps) => {
-    const info = useSelf((me) => me.info);
+    // View user metadata
+    // const info = useSelf((me) => me.info);
 
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
-            <Info />
+            <Info boardId={boardId} />
             <Participants />
             <Toolbar />
        </main>
--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-export const Info = () => {
+interface InfoProps {
+    boardId: string;
+
+}
+
+export const Info = ({
+    boardId
+}: InfoProps) => {
+    const oard =
+
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
            TODO: Information about the board
