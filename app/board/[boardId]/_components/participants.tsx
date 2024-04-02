d663015b48b3e0ec36200da432d04a1c242cf013
ansorokey <ansorokey@gmail.com>
2024-04-02 19:20:46 -0400

    other user avatars appear in list on board

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { UserAvatar } from "./user-avatar";
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+const MAX_SHOWN_USERS = 2;
+
 export const Participants = () => {
+    const users = useOthers();
+    const currrentUsers = useSelf();
+    const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
     return (
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-            List of users
+            <div className="flex gap-x-2">
+                {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info}) => {
+                    return (
+                        <UserAvatar
+                            key={connectionId}
+                            src={info?.picture}
+                            name={info?.name}
+                            fallback={info?.name?.[0] || "A"}
+                        />
+                    );
+                })}
+
+            </div>
         </div>
     );
 };
