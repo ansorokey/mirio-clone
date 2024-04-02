9720f1dfc72a06e5f2c1897bc137465832bfd958
ansorokey <ansorokey@gmail.com>
2024-04-02 13:07:18 -0400

    refactored skeletons into sub components

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -8,8 +8,9 @@ export const Info = () => {
     );
 };
 
-
-Info.Skeleton = function InfoSkeleton() {
+// The parent component that uses this either has to be marked as use client
+// OR instead of adding this skeleton as a property, export it as its own sub-component
+export const InfoSkeleton = () => {
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12
         flex items-center shadow-md w-[300px]" />
