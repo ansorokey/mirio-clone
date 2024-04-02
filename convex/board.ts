540f743c4261d361a71a856e91111213324c0a7f
ansorokey <ansorokey@gmail.com>
2024-04-02 10:12:40 -0400

    board get query

--- convex/board.ts
+++ convex/board.ts
@@ -1,17 +1,17 @@
 import { v } from "convex/values";
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 const images = [
-    "/placeholders/1 (1).svg",
-    "/placeholders/2 (1).svg",
-    "/placeholders/3 (1).svg",
-    "/placeholders/4 (1).svg",
-    "/placeholders/5 (1).svg",
-    "/placeholders/6 (1).svg",
-    "/placeholders/7 (1).svg",
-    "/placeholders/8 (1).svg",
-    "/placeholders/9 (1).svg",
-    "/placeholders/10 (1).svg",
+    "/placeholders/1.svg",
+    "/placeholders/2.svg",
+    "/placeholders/3.svg",
+    "/placeholders/4.svg",
+    "/placeholders/5.svg",
+    "/placeholders/6.svg",
+    "/placeholders/7.svg",
+    "/placeholders/8.svg",
+    "/placeholders/9.svg",
+    "/placeholders/10.svg",
 ]
 
 export const create = mutation({
@@ -170,6 +170,16 @@ export const unfavorite = mutation({
         }
 
         await ctx.db.delete(existingFavorite._id);
+
+        return board;
+    }
+});
+
+// GET the current board for authentication
+export const get = query({
+    args: { id: v.id("boards")},
+    handler: async (ctx, args) => {
+        const board = await ctx.db.get(args.id);
 
         return board;
     }
