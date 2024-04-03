cf0d643eea47221f47fb1006000a07c8ddbe309c
ansorokey <ansorokey@gmail.com>
2024-04-02 20:23:02 -0400

    passing canvas state props to toolbar

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useState } from "react";
+import { CanvasState, CanvasMode } from "@/types/canvas";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+// import { useSelf } from "@/liveblocks.config";
 
-import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
     boardId: string;
@@ -16,11 +19,26 @@ export const Canvas = ({
     // View user metadata
     // const info = useSelf((me) => me.info);
 
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
+
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
        </main>
     );
 }
