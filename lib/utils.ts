a0de99eeae7676dda6b8183ccb18a6bda74142fe
ansorokey <ansorokey@gmail.com>
2024-04-03 13:02:20 -0400

    pointer event util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
+import { Camera } from "@/types/canvas"
 import { type ClassValue, clsx } from "clsx"
+import React from "react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -21,6 +23,18 @@ const COLORS = [
   "#6E60A0"
 ]
 
+// Returns a color from the above array based ona user's connectionId for a board/live room
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length]
+}
+
+// Calculates the coordinates for the camera
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY)- camera.y,
+  }
 }
