3efecc84ce1da342e05491ce00060ed75621fc67
ansorokey <ansorokey@gmail.com>
2024-04-03 18:52:07 -0400

    inserting layer

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React, { useCallback, useState } from "react";
-import { CanvasState, CanvasMode, Camera } from "@/types/canvas";
+import { CanvasState, CanvasMode, Camera, Color, LayerType, Point } from "@/types/canvas";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useCanRedo, useCanUndo, useHistory, useMutation } from "@/liveblocks.config";
+import { useCanRedo, useCanUndo, useHistory, useMutation, useStorage } from "@/liveblocks.config";
 import { CursorsPresence } from "./cursors-presence";
 import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { nanoid } from "nanoid";
+import { LiveObject } from "@liveblocks/client";
 // import { useSelf } from "@/liveblocks.config";
 
 
@@ -20,17 +22,47 @@ const MAX_LAYERS = 100;
 export const Canvas = ({
     boardId
 }: CanvasProps) => {
-    // View user metadata
-    // const info = useSelf((me) => me.info);
+    const layerIds = useStorage((root) => root.layerIds);
 
     const [camera, setCamera] = useState<Camera>({x:0, y:0});
     const [canvasState, setCanvasState] = useState<CanvasState>({
         mode: CanvasMode.None,
     });
+    const [lastUsedColor, setLastUsedColor] = useState<Color>({ r:0, g:0, b:0 });
 
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const insertLayer = useMutation((
+        {storage, setMyPresence},
+        layerType: LayerType.Ellipse | LayerType.Rectangle | LayerType.Text | LayerType.Note,
+        position: Point
+    ) => {
+        const liveLayers = storage.get("layers");
+
+        if(liveLayers.size >= MAX_LAYERS) {
+            return;
+        }
+
+        const liveLayerIds = storage.get("layerIds");
+        const layerId = nanoid();
+        const layer = new LiveObject({
+            typr: layerType,
+            x: position.x,
+            y: position.y,
+            height: 100,
+            width: 100,
+            fill: lastUsedColor
+        });
+
+        liveLayerIds.push(layerId);
+        liveLayers.set(layerId, layer);
+
+        setMyPresence({ selection: [layerId]}, {addToHistory: true});
+        setCanvasState({ mode: CanvasMode.None });
+    }, [lastUsedColor]);
+
     const onWheel = useCallback((e: React.WheelEvent) => {
         setCamera((camera) => {
             return {
