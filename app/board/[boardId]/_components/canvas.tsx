9647c0aefb580b7578077bda2fec301337a96dbd
ansorokey <ansorokey@gmail.com>
2024-04-04 13:00:13 -0400

    fixed bug with live selections: added selections to layerIdsToColorSelection dependancy array

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -151,7 +151,7 @@ export const Canvas = ({
         }
 
         return layerIdsToColorSelection;
-    }, []);
+    }, [selections]);
 
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
