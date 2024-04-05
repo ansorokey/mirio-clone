17b9dae99658c4994382aaf8e1906a34ce6c1e82
ansorokey <ansorokey@gmail.com>
2024-04-04 20:09:09 -0400

    current user can now select layers

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf, useStorage } from "@/liveblocks.config";
 import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
@@ -21,10 +22,23 @@ export const SelectionBox = memo(({
         soleLayerId && root.layers.get(soleLayerId)?.type !== LayerType.Path
     );
 
+    const bounds = useSelectionBounds();
+
+    if(!bounds) return null;
+
     return (
-        <div>
-            Box
-        </div>
+        <>
+            <rect
+                className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+                style={{
+                    transform: `translate(${bounds.x}px, ${bounds.y}px)`
+                }}
+                x={0}
+                y={0}
+                width={bounds.width}
+                height={bounds.height}
+            />
+        </>
     );
 });
 
