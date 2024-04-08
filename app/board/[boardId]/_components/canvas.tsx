40ccb7763ab1ad73da3024dcb080e6d3161580cf
ansorokey <ansorokey@gmail.com>
2024-04-08 13:32:20 -0400

    defined translateSelectedLayers function

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -78,6 +78,34 @@ export const Canvas = ({
         setCanvasState({ mode: CanvasMode.None });
     }, [lastUsedColor]);
 
+    const translateSelectedLayers = useMutation((
+        {storage, self},
+        point: Point
+    ) => {
+        if(canvasState.mode !== CanvasMode.Translating) return;
+
+        const offset = {
+            x: point.x - canvasState.current.x,
+            y: point.y - canvasState.current.y,
+        }
+
+        const liveLayers = storage.get("layers");
+
+        for(const id of self.presence.selection) {
+            const layer = liveLayers.get("id");
+
+            if(layer) {
+                layer.update({
+                    x: layer.get("x") + offset.x,
+                    y: layer.get("y") + offset.y
+                });
+            };
+        }
+
+        setCanvasState({ mode: CanvasMode.Translating, current: point});
+
+    }, [canvasState]);
+
     const resizeSelectedLayer = useMutation((
         { storage, self},
         point: Point,
@@ -124,7 +152,9 @@ export const Canvas = ({
 
         const current = pointerEventToCanvasPoint(e, camera);
 
-        if(canvasState.mode === CanvasMode.Resizing) {
+        if(canvasState.mode === CanvasMode.Translating) {
+            console.log('translating');
+        } else if(canvasState.mode === CanvasMode.Resizing) {
             resizeSelectedLayer(current);
         }
 
