920ab535bd5e31ac74310b13ae2a65dacd288dc7
ansorokey <ansorokey@gmail.com>
2024-04-08 13:25:35 -0400

    fixed resize bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -43,6 +43,27 @@ export function colorToCss(color: Color) {
   return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`
 }
 
+
+/*
+  Originally had an issue with the code below where grabbing any corner/side would default to the bottom right corner.
+  The cause was using && instead of &.
+  & is the bitwise operator.
+  This means it converts whatever it is comparing into binary numbers.
+  Then, it compares the positions of 1's between the two.
+  ANYWHERE where there is a 1, it is carried over into the result.
+  So [00001111](15) & [11111111](255) = [11111111](255).
+  The bitwise operator will typecast inputs into binary numebrs and return a binary number.
+
+  && is the logical AND operartion.
+  It checks for truthy and falsy values.
+  A comparison made using && returns either the first falsey value OR the last truthy value.
+
+  The reason we wanted to use the bitwise operator here is because we aren't just checking for truthy or falsy.
+  We are specifically checking to see if the two values equate to the matching Side.[Side]
+  (false && true) == 'Left' wasw what we were originally doing, where as
+  ([0101] & [1101] == [1101])  is what we wanted
+
+*/
 export function resizeBounds(
   bounds: XYWH,
   corner: Side,
@@ -55,22 +76,22 @@ export function resizeBounds(
     height: bounds.height
   };
 
-  if((corner && Side.Left) === Side.Left) {
+  if((corner & Side.Left) === Side.Left) {
     result.x = Math.min(point.x, bounds.x + bounds.width);
     result.width = Math.abs(bounds.x + bounds.width - point.x);
   }
 
-  if((corner && Side.Right) === Side.Right) {
+  if((corner & Side.Right) === Side.Right) {
     result.x = Math.min(point.x, bounds.x);
     result.width = Math.abs(point.x - bounds.x);
   }
 
-  if((corner && Side.Top) === Side.Top) {
+  if((corner & Side.Top) === Side.Top) {
     result.y = Math.min(point.y, bounds.y + bounds.height);
     result.height = Math.abs(bounds.y + bounds.height - point.y);
   }
 
-  if((corner && Side.Bottom) === Side.Bottom) {
+  if((corner & Side.Bottom) === Side.Bottom) {
     result.y = Math.min(point.y, bounds.y);
     result.height = Math.abs(point.y - bounds.y);
   }
