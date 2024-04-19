ca4e775a1c3d1337834f6567fac892470522ad6b
ansorokey <ansorokey@gmail.com>
2024-04-19 18:23:57 -0400

    fixed typo on env variable

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,7 +6,7 @@ import { auth, currentUser } from "@clerk/nextjs";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret: process.env.LIBEBLOCKS_SECRET_KEY!
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!
 });
 
 export async function POST(request: Request) {
